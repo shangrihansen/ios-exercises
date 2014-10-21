23ee9be7d825ea70cee8b88c836d0b60d41a7325
Colt Hansen <shangrihansen@gmail.com>
2014-10-20 19:05:28 -0700

    completed swift playground exercises

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,7 +8,7 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    return "My favorite cheese is \(cheese)."
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -23,10 +23,18 @@ Arrays & Dictionaries
 let numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+var mutableArray = numberArray
+mutableArray.append(5)
+
+mutableArray
 
 let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+var mutableDict = numberDictionary
+mutableDict[5] = "five"
+
+mutableDict
 
 /*
 
@@ -36,9 +44,15 @@ Loops
 
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for closedRange in 1...10 {
+    println(closedRange)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for halfClosedRange in 1..<11 {
+    println(halfClosedRange)
+}
 
 let worf = [
     "name": "Worf",
@@ -53,12 +67,22 @@ let picard = [
     "information": "Captain of the USS Enterprise",
     "favorite drink": "tea, Earl Grey, hot"]
 
-let characters = [worf, picard]
+let colt = [
+    "name": "Colt Hansen",
+    "rank": "admiral",
+    "information": "Talks in his sleep",
+    "favorite drink": "hot cocoa, straight up"]
+
+let characters = [worf, picard, colt]
 
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+    var faveDrinks = [String] ()
+    for key in characters {
+        faveDrinks.append(key["favorite drink"]!)
+    }
+    return faveDrinks
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -74,10 +98,21 @@ Functions
 // Make a function that inputs an array of strings and outputs the strings separated by a semicolon
 
 let strings = ["milk", "eggs", "bread", "challah"]
+let beasties = ["no", "sleep", "til", "Brooklyn!"]
 
 // WORK HERE - make your function and pass `strings` in
+// I know, not the best name. I couldn't help myself...
+func innieOuttieSemi(innieArray:Array<String>) -> String {
+    let outtie = ";".join(innieArray)
+    return outtie
+}
 
+innieOuttieSemi(beasties)
+let expectedOutput2 = "no;sleep;til;Brooklyn!"
+
+innieOuttieSemi(strings)
 let expectedOutput = "milk;eggs;bread;challah"
+
 
 /*
 
@@ -89,3 +124,4 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+let closureSortedAlpha = sorted(cerealArray, <)
